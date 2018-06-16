a3966fb59af6b85ad7a26cb8a21b02373ea42739
Stormfluger <turkom76@mail.ru>
2018-06-16 14:30:07 +0500

    work

---
+++ ra-homeworks-master/jsx/menu/js/Menu.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const Menu = ({items, opened}) => {
+  const menuItems = items.map((item, index) => <li><a href={item.href}>{item.title}</a></li>);
+  const menu = (<nav><ul>{ menuItems }</ul></nav>);
+  return (
+    <div className={ "menu" + (opened ? ' menu-open' : '') }>
+      <div className="menu-toggle"><span/></div>
+      { (opened) ? menu : ''}
+    </div>
+  );
+};
