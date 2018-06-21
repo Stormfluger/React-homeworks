d611d2d46819e1e7f0ed72262ddf3de1bd43f812
Stormfluger <turkom76@mail.ru>
2018-06-21 10:49:09 +0500

    work

--- ra-homeworks-master/events/auth/js/AuthForm.js
+++ ra-homeworks-master/events/auth/js/AuthForm.js
@@ -1,1 +1,47 @@
 'use strict';
+
+function AuthForm({onAuth}) {
+    const fields = {
+        name: null,
+        email: null,
+        password: null
+    };
+
+    const checkData = {
+        password: /[^a-z0-9_]/ig,
+        email: /[^a-z0-9_@\-\.]/ig
+    };
+
+    const authUser = () => {
+        event.preventDefault();
+        onAuth && typeof(onAuth) === 'function' &&
+        onAuth({
+            name: fields.name.value,
+            email: fields.email.value,
+            password: fields.password.value
+        });
+    };
+
+    const verifyUser = fieldsName => fields[fieldsName].value = fields[fieldsName].value.replace(checkData[fieldsName], '');
+
+    return (
+        <form className="ModalForm" action="/404/authUser/" method="POST" onSubmit={authUser}>
+            <div className="Input">
+                <input required type="text" placeholder="Имя" ref={element => fields.name = element}/>
+                <label></label>
+            </div>
+            <div className="Input">
+                <input type="email" placeholder="Электронная почта" ref={element => fields.email = element} onChange={() => verifyUser('email')}/>
+                <label></label>
+            </div>
+            <div className="Input">
+                <input required type="password" placeholder="Пароль" ref={element => fields.password = element} onChange={() => verifyUser('password')}/>
+                <label></label>
+            </div>
+            <button type="submit">
+                <span>Войти</span>
+                <i className="fa fa-fw fa-chevron-right"></i>
+            </button>
+        </form>
+    );
+}
