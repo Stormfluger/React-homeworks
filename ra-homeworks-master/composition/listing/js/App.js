afc75e5cd3f6311887687efb7b1acd9e914cb63c
Stormfluger <turkom76@mail.ru>
2018-06-25 16:25:37 +0500

    work

--- ra-homeworks-master/composition/listing/js/App.js
+++ ra-homeworks-master/composition/listing/js/App.js
@@ -2,15 +2,17 @@
 
 const App = ({items}) => (
   <main>
-    {items.map(item => {
-      switch(item.type) {
-        case 'unisex':
-          return <Item color="black" item={item} />;
-        case 'male':
-          return <Item color="blue" item={item} />;
-        case 'female':
-          return <Item color="orange" item={item} />;
-      }
-    })}
+    {items.map(item => <colorItem item={item} />)}
   </main>
 );
+
+const colorItem = ({item}) => {
+  switch(item.type) {
+    case 'unisex':
+      return <Item color="black" item={item} />;
+    case 'male':
+      return <Item color="blue" item={item} />;
+    case 'female':
+      return <Item color="orange" item={item} />;
+  }
+};
