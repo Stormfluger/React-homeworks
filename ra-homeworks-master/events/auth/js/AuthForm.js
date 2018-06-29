c9ded27d6751431d96e9951205ca9e76aa73dc0b
Stormfluger <turkom76@mail.ru>
2018-06-29 15:08:27 +0500

    Доработка1

--- ra-homeworks-master/events/auth/js/AuthForm.js
+++ ra-homeworks-master/events/auth/js/AuthForm.js
@@ -1,47 +1,45 @@
 'use strict';
 
-function AuthForm({onAuth}) {
-    const fields = {
-        name: null,
-        email: null,
-        password: null
-    };
-
-    const checkData = {
-        password: /[^a-z0-9_]/ig,
-        email: /[^a-z0-9_@\-\.]/ig
-    };
-
-    const authUser = () => {
-        event.preventDefault();
-        onAuth && typeof(onAuth) === 'function' &&
-        onAuth({
+const AuthForm = onAuth => {
+ 
+  const authUser = event => {
+    event.preventDefault();
+    if ((!onAuth) || (typeof onAuth !== 'function')) return null;
+    const fields = event.currentTarget.elements;
+ 
+        const userAuth = {  
             name: fields.name.value,
             email: fields.email.value,
             password: fields.password.value
-        });
-    };
-
-    const verifyUser = fieldsName => fields[fieldsName].value = fields[fieldsName].value.replace(checkData[fieldsName], '');
-
-    return (
-        <form className="ModalForm" action="/404/authUser/" method="POST" onSubmit={authUser}>
-            <div className="Input">
-                <input required type="text" placeholder="Имя" ref={element => fields.name = element}/>
-                <label></label>
-            </div>
-            <div className="Input">
-                <input type="email" placeholder="Электронная почта" ref={element => fields.email = element} onChange={() => verifyUser('email')}/>
-                <label></label>
-            </div>
-            <div className="Input">
-                <input required type="password" placeholder="Пароль" ref={element => fields.password = element} onChange={() => verifyUser('password')}/>
-                <label></label>
-            </div>
-            <button type="submit">
-                <span>Войти</span>
-                <i className="fa fa-fw fa-chevron-right"></i>
-            </button>
-        </form>
-    );
+        };
+        onAuth(userAuth);
+  }
+  
+  function checkEmail(event) {
+        event.currentTarget.value = event.currentTarget.value.replace(/[^\w@\.-]+/gi, "");
+  }
+    
+  function checkPassword(event) {
+        event.currentTarget.value = event.currentTarget.value.replace(/[^\w]+/gi, "");
+  }
+  return (
+    <form className="ModalForm" onSubmit={authUser} action="/404/auth/" method="POST">
+      <div className="Input">
+        <input required type="text" placeholder="Имя" name="name" />
+        <label></label>
+      </div>
+      <div className="Input">
+        <input onChange={checkEmail} type="email" placeholder="Электронная почта" name="email" />
+        <label></label>
+      </div>
+      <div className="Input">
+        <input onChange={checkPassword} required type="password" placeholder="Пароль" name="password" />
+        <label></label>
+      </div>
+      <button type="submit">
+        <span>Войти</span>
+        <i className="fa fa-fw fa-chevron-right"></i>
+      </button>      
+    </form>
+  );
 }
--- ra-homeworks-master/events/feedback/js/FeedbackForm.js
+++ ra-homeworks-master/events/feedback/js/FeedbackForm.js
@@ -1,69 +1,135 @@
 'use strict';
 
-function FeedbackForm({data, onSubmit}) {
-    let nameField,
-        emailField,
-        subjectField,
-        messageField,
-        form;
+const FeedbackForm = props => {
 
-    const choiceForm = event => {
-        event.preventDefault();
-        let salutationField = form.querySelector('input[type=radio]:checked');
-        let snacksFields = form.querySelectorAll('input[type=checkbox]:checked');
-        let snacks = Array.from(snacksFields).map(item => item.value);
-        const result = {
-            [nameField.name]: nameField.value,
-            [emailField.name]: emailField.value,
-            [subjectField.name]: subjectField.value,
-            [messageField.name]: messageField.value,
-            [salutationField.name]: salutationField.value,
-            snacks: snacks
-        };
-        onSubmit(JSON.stringify(result));
-    };
+	const choiceForm = event => {
+		event.preventDefault();
+		const formElements = event.currentTarget.elements;
+		console.log(formElements.salutation.value);
+		const form = {
+			salutation: formElements.salutation.value,
+			name: formElements.name.value,
+			subject: formElements.subject.value,
+			message: formElements.message.value,
+			email: formElements.email.value,
+			snacks: Array.from(formElements.snacks).filter(elem => elem.checked).map(elem => elem.value)
+		}
+		props.onSubmit(JSON.stringify(form));
+    }
 
     return (
-        <form className="content__form contact-form" ref={element => form = element} onSubmit={choiceForm}>
-            <div className="testing">
-                <p>Чем мы можем помочь?</p>
-            </div>
-            <div className="contact-form__input-group">
-                <input className="contact-form__input contact-form__input--radio" id="salutation-mr" name="salutation" type="radio" value="Мистер" defaultChecked={data.salutation === 'Мистер'}/>
-                <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mr">Мистер</label>
-                <input className="contact-form__input contact-form__input--radio" id="salutation-mrs" name="salutation" type="radio" value="Мисис" defaultChecked={data.salutation === 'Мисис'}/>
-                <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mrs">Мисис</label>
-                <input className="contact-form__input contact-form__input--radio" id="salutation-ms" name="salutation" type="radio" value="Мис" defaultChecked={data.salutation === 'Мис'}/>
-                <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-ms">Мис</label>
-            </div>
-            <div className="contact-form__input-group">
-                <label className="contact-form__label" htmlFor="name">Имя</label>
-                <input className="contact-form__input contact-form__input--text" id="name" name="name" type="text" defaultValue={data.name} ref={element => nameField = element}/>
-            </div>
-            <div className="contact-form__input-group">
-                <label className="contact-form__label" htmlFor="email">Адрес электронной почты</label>
-                <input className="contact-form__input contact-form__input--email" id="email" name="email" type="email" defaultValue={data.email} ref={element => emailField = element}/>
-            </div>
-            <div className="contact-form__input-group">
-                <label className="contact-form__label" htmlFor="subject">Чем мы можем помочь?</label>
-                <select className="contact-form__input contact-form__input--select" id="subject" name="subject" defaultValue={data.subject} ref={element => subjectField = element}>
-                    <option>У меня проблема</option>
-                    <option>У меня важный вопрос</option>
-                </select>
-            </div>
-            <div className="contact-form__input-group">
-                <label className="contact-form__label" htmlFor="message">Ваше сообщение</label>
-                <textarea className="contact-form__input contact-form__input--textarea" id="message" name="message" rows="6" cols="65" defaultValue={data.message} ref={element => messageField = element}></textarea>
-            </div>
-            <div className="contact-form__input-group">
-                <p className="contact-form__label--checkbox-group">Хочу получить:</p>
-                <input className="contact-form__input contact-form__input--checkbox" id="snacks-pizza" name="snacks" type="checkbox" value="пицца" defaultChecked={data.snacks && data.snacks.includes('пицца')}/>
-                <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-pizza">Пиццу</label>
-                <input className="contact-form__input contact-form__input--checkbox" id="snacks-cake" name="snacks" type="checkbox" value="пирог" defaultChecked={data.snacks && data.snacks.includes('пирог')}/>
-                <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-cake">Пирог</label>
-            </div>
-            <button className="contact-form__button" type="submit">Отправить сообщение!</button>
-            <output id="result" />
-        </form>
+      <form className="content__form contact-form" onSubmit={choiceForm}>
+		<div className="testing">
+		  <p>Чем мы можем помочь?</p>
+		</div>
+		<div className="contact-form__input-group">
+		  <input 
+		    defaultChecked={props.data.salutation === "Мистер"} 
+			className="contact-form__input contact-form__input--radio" 
+			id="salutation-mr" 
+			name="salutation" 
+			type="radio" 
+			value="Мистер"
+		  />
+		  
+		  <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mr">Мистер</label>
+
+		  <input 
+			defaultChecked={props.data.salutation === "Мисис"} 
+			className="contact-form__input contact-form__input--radio" 
+			id="salutation-mrs" 
+			name="salutation" 
+			type="radio" 
+			value="Мисис"
+		  />
+
+		  <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mrs">Мисис</label>
+
+		  <input 
+			defaultChecked={props.data.salutation === "Мис"} 
+			className="contact-form__input contact-form__input--radio" 
+			id="salutation-ms" 
+			name="salutation" 
+			type="radio" 
+			value="Мис"
+		  />
+
+		  <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-ms">Мис</label>
+		</div>
+
+		<div className="contact-form__input-group">
+		  <label className="contact-form__label" htmlFor="name">Имя</label>
+
+		  <input 
+			className="contact-form__input contact-form__input--text" 
+			defaultValue={props.data.name} 
+			id="name" 
+			name="name" 
+			type="text"
+		  />
+		</div>
+
+		<div className="contact-form__input-group">
+		  <label className="contact-form__label" htmlFor="email">Адрес электронной почты</label>
+		  <input 
+			className="contact-form__input contact-form__input--email" 
+			defaultValue={props.data.email} 
+			id="email" 
+			name="email" 
+			type="email"
+		/>
+		</div>
+
+		<div className="contact-form__input-group">
+		  <label className="contact-form__label" htmlFor="subject">Чем мы можем помочь?</label>
+		  <select 
+			className="contact-form__input contact-form__input--select" 
+			defaultValue={props.data.subject} 
+			id="subject" 
+			name="subject"
+		  >
+		  <option>У меня проблема</option>
+			<option>У меня важный вопрос</option>
+		  </select>
+		</div>
+
+		<div className="contact-form__input-group">
+		  <label className="contact-form__label" htmlFor="message">Ваше сообщение</label>
+		  <textarea 
+			className="contact-form__input contact-form__input--textarea" 
+			defaultValue={props.data.message} 
+			id="message" 
+			name="message" 
+			rows="6" 
+			cols="65"
+		  >
+		  </textarea>
+		</div>
+	    <div className="contact-form__input-group">
+		  <p className="contact-form__label--checkbox-group">Хочу получить:</p>
+
+		  <input 
+			className="contact-form__input contact-form__input--checkbox" 
+			defaultChecked={props.data.snacks.includes('пицца')} 
+			id="snacks-pizza" 
+			name="snacks" 
+			type="checkbox" 
+			value="пицца"
+		  />
+		  <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-pizza">Пиццу</label>
+
+		  <input 
+			className="contact-form__input contact-form__input--checkbox" 
+			defaultChecked={props.data.snacks.includes('пирог')} 
+			id="snacks-cake" 
+			name="snacks" 
+			type="checkbox" 
+			value="пирог"
+		  />
+		  <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-cake">Пирог</label>
+		</div>
+		<button className="contact-form__button" type="submit">Отправить сообщение!</button>
+		<output id="result" />
+      </form>
     );
 }
